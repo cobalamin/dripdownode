4be5962a6e35f2c6d81f61e4c7a1de7d8b429024
Simon Welker <simon.welker@outlook.com>
2014-11-23 14:52:49 +0100

    New build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,69 @@ module.exports = function(grunt) {
 				tasks: ['sass'],
 				options: { spawn: false }
 			}
-		}
+		},
+
+		copy: {
+			build: {
+				files: [
+					{ expand: true, src: ['dist/**'], dest: 'built_app/' },
+					{ expand: true, src: ['components/**'], dest: 'built_app/' },
+					{ expand: true, src: ['fonts/**'], dest: 'built_app/' },
+					{ expand: true, src: ['images/**'], dest: 'built_app/' },
+					{ expand: true, src: ['node_modules/**'], dest: 'built_app/' },
+					{ expand: true, src: ['server/**'], dest: 'built_app/' },
+					{ expand: true, src: ['templates/**'], dest: 'built_app/' },
+
+					{ expand: true, src: ['atom-app.html'], dest: 'built_app/' },
+					{ expand: true, src: ['atom-app.js'], dest: 'built_app/' },
+					{ expand: true, src: ['package.json'], dest: 'built_app/' },
+					{ expand: true, src: ['LICENSE'], dest: 'built_app/' }
+				],
+			},
+		},
+
+		clean: {
+			build: ['built_app/node_modules/grunt*'],
+			build_after: ['built_app/*']
+		},
+
+		'build-atom-shell-app': {
+			options: {
+				platforms: ['darwin', 'win32', 'linux'],
+				app_dir: 'built_app'
+			}
+		},
+
+		chmod: {
+			options: {
+				mode: '755'
+			},
+			build: {
+				src: [
+					'build/linux/atom-shell/atom',
+					'build/darwin/atom-shell/Atom.app/Contents/MacOS/Atom'
+				]
+			}
+		},
+
+		rename: {
+			osx: {
+				src: 'build/darwin/atom-shell/Atom.app',
+				dest: 'build/darwin/atom-shell/dripdownode.app'
+			},
+			linux: {
+				src: 'build/linux/atom-shell/atom',
+				dest: 'build/linux/atom-shell/dripdownode',
+			},
+			windows: {
+				src: 'build/win32/atom-shell/atom.exe',
+				dest: 'build/win32/atom-shell/dripdownode.exe',
+			}
+		},
+
+		auto_install: {
+			local: {}
+		},
 	});
 
 	[
@@ -51,11 +113,31 @@ module.exports = function(grunt) {
 		'grunt-contrib-concat',
 		'grunt-contrib-sass',
 		'grunt-contrib-watch',
-		'grunt-notify'
+		'grunt-notify',
+		'grunt-contrib-copy',
+		'grunt-contrib-clean',
+		'grunt-chmod',
+		'grunt-rename',
+		'grunt-contrib-compress',
+		'grunt-auto-install',
+		'grunt-atom-shell-app-builder'
 	].forEach(function(task) { grunt.loadNpmTasks(task); });
 
 	var allButWatch = ['jshint', 'concat', 'sass'];
 	var all = allButWatch.concat('watch');
 	grunt.registerTask('nowatch', allButWatch);
 	grunt.registerTask('default', all);
+
+	grunt.registerTask('build', allButWatch.concat(
+		'auto_install',
+		'copy:build',
+		'clean:build',
+		'build-atom-shell-app'
+	));
+
+	grunt.registerTask('package', [
+		'chmod:build',
+		'rename',
+		'clean:build_after'
+	]);
 };
